70426793c784dec7ee07ee56299fd24db04ba253
Gibon_david <[My\_gibon.david@live.fr](mailto:MY_gibon.david@live.fr)>
2023-05-27 09:51:58 +0200

    commit quete20 ok

--- src/Controller/ActorController.php
+++ src/Controller/ActorController.php
@@ -4,11 +4,15 @@ namespace App\Controller;
 
 use App\Entity\Actor;
 use App\Entity\Program;
+use App\Form\ActorType;
 use App\Repository\ActorRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Entity;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 #[Route('/actor', name: 'actor_')]
 class ActorController extends AbstractController
@@ -32,4 +36,21 @@ class ActorController extends AbstractController
 
         ]);
     }
+#[Route('/new', name: 'new')]
+    public function new(Request $request, ActorRepository $actorRepository,  )
+{
+    $actor = new Actor();
+    $form = $this->createForm(ActorType::class, $actor);
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+        $actorRepository->save($actor, true);
+
+        $this->addFlash('success', 'the message à bien été crée');
+
+        return $this->redirectToRoute('actor_index');
+    }
+
+    return $this->render('actor/new.html.twig', ['form'=> $form]);
 }
+    }
